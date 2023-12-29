39b449cc0dfe7618181c38620f1705b9c42eb46c
Juan Nogueira <juannogueiraxavier@gmail.com>
2023-12-28 22:48:34 -0300

    Generics - Type safety

---
+++ Generics 1/src/App.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+		PrintService<Integer> ps = new PrintService<>();
+		
+		System.out.print("How many values? ");
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			Integer value = sc.nextInt();
+			ps.addValue(value);
+		}
+
+		ps.print();
+		Integer x = ps.first();
+		System.out.println("First: " + x);
+		
+		sc.close();
+    }
+}
