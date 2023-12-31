f1dc19d85ba7f05f69d20c98c8333de0dd4cf323
Juan Nogueira <juannogueiraxavier@gmail.com>
2023-12-30 23:53:06 -0300

    default methods

---
+++ Interface 5/src/entities/BrazilInterestService.java
@@ -0,0 +1,13 @@
+public class BrazilInterestService implements InterestService {
+
+	private double interestRate;
+
+	public BrazilInterestService(double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	@Override
+	public double getInterestRate() {
+		return interestRate;
+	}
+}
---
+++ Interface 5/src/entities/UsaInterestService.java
@@ -0,0 +1,13 @@
+public class UsaInterestService implements InterestService {
+
+	private double interestRate;
+
+	public UsaInterestService(double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	@Override
+	public double getInterestRate() {
+		return interestRate;
+	}
+}
