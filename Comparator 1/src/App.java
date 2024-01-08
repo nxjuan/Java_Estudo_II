21a2d83cb70ed628177cde15a130166f745dd199
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-01-08 11:28:30 -0300

    Introdução a comparator e expreções lambda - pt 3 -Classe anonima

--- Comparator 1/src/App.java
+++ Comparator 1/src/App.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class App {
@@ -10,7 +11,16 @@ public class App {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 
-		list.sort(new MyComparator());
+		Comparator<Product> compare = new Comparator<Product>(){
+
+			@Override
+			public int compare(Product o1, Product o2) {
+				return o1.getName().toUpperCase().compareTo(o2.getName().toUpperCase());
+			}
+			
+		};
+
+	list.sort(compare);
 
 		for (Product p : list) {
 			System.out.println(p);
