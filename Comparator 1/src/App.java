9c0da691299f31f5d0d8d16e350fa46dca609bae
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-01-08 11:06:34 -0300

    Introdução a comparator e expreções lambda - pt 2

--- Comparator 1/src/App.java
+++ Comparator 1/src/App.java
@@ -10,7 +10,7 @@ public class App {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 
-		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+		list.sort(new MyComparator());
 
 		for (Product p : list) {
 			System.out.println(p);
