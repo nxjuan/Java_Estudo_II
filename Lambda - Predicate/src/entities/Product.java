5f11e13a02aa2f4653c2ad1cbaebdb9934a08ad9
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-01-09 12:57:41 -0300

    Expreções lambda com Predicate - referencia com metodo NÃO estatico

--- Lambda - Predicate/src/entities/Product.java
+++ Lambda - Predicate/src/entities/Product.java
@@ -26,8 +26,8 @@ public class Product {
 		this.price = price;
 	}
 
-    public static boolean staticProductPredicate(Product p) {
-		return p.getPrice() >= 100.0;
+    public boolean nonStaticProductPredicate() {
+		return price >= 100.0;
 	}
 
 	@Override
