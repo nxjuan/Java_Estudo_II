eae2aec7c4bf0a29888b753fbc11a1bde5b50db1
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-01-09 12:53:16 -0300

    Expreções lambda com Predicate

---
+++ Lambda - Predicate/src/aplication/App.java
@@ -0,0 +1,24 @@
+package aplication;
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+import util.ProductPredicate;
+
+public class App {
+    public static void main(String[] args) {
+
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+
+		list.removeIf(new ProductPredicate());
+
+		for (Product p : list) {
+			System.out.println(p);
+		}
+	}
+}
